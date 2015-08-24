c15afcf78ccc35953f6d2e8cbee1f504056eccbf
Jenda <jenda.jzqk73@hrach.eu>
2015-08-24 14:23:52 +0200

    move revbits to extra file

--- delta.c
+++ delta.c
@@ -15,6 +15,8 @@
 
 #include <linux/fs.h>
 
+#include "revbits.h"
+
 // XXX put into .h
 typedef struct blockspec {
   long blockno;
@@ -127,17 +129,6 @@ int max(int a, int b) {
   } else {
     return b;
   }
-}
-
-/* Reverse bits in u64 (ok, it should be a optimized) */
-uint64_t rev(uint64_t r) {
-	uint64_t r1 = r;
-	uint64_t r2 = 0;
-	for (int j = 0; j < 64 ; j++ ) {
-		r2 = (r2<<1) | (r1 & 0x01);
-		r1 = r1 >> 1;
-	}
-	return r2;
 }
 
 /* Index of blockstarts in table XXX explain */
