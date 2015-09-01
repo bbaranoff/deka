b502eb4c72525b77a1b6eb9c08234e89187e2274
Jenda <jenda.jzqk73@hrach.eu>
2015-09-02 00:53:41 +0200

    init script

---
+++ init.sh
@@ -0,0 +1,11 @@
+./paplon.py > paplon.log &
+
+sleep 1
+
+./delta_client.py > delta_client1.log &
+./delta_client.py > delta_client2.log &
+
+PYOPENCL_CTX=0 ./oclvankus.py > oclvankus1_1.log &
+PYOPENCL_CTX=1 ./oclvankus.py > oclvankus2_1.log &
+PYOPENCL_CTX=2 ./oclvankus.py > oclvankus3_1.log &
+
