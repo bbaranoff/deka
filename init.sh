20f05823ff037fd7ce59140af88ef8d32796bc5f
Jenda <jenda.jzqk73@hrach.eu>
2017-01-13 11:28:55 +0100

    python3.5; comments in init.sh

--- init.sh
+++ init.sh
@@ -2,11 +2,12 @@
 
 sleep 1
 
+# we run a few delta lookups to achieve paralelism
 ./delta_client.py > delta_client1.log &
 ./delta_client.py > delta_client2.log &
 ./delta_client.py > delta_client3.log &
-# XXX
 
+# and one or two ocl computations per GPU (because it takes ~200 ms for the GPU to process and then another ~30 ms for the host to create new GPU blob and we want to use the time inbetween, so we run two opencl queues
 PYOPENCL_CTX=0 ./oclvankus.py > oclvankus1_1.log &
 PYOPENCL_CTX=0 ./oclvankus.py > oclvankus1_2.log &
 PYOPENCL_CTX=1 ./oclvankus.py > oclvankus2_1.log &
